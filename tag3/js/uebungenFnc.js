7468856153cb3be6f65ab477466d8a7491e98555
aviareps_pwolf <pwolf@aviareps.com>
2025-01-15 08:55:12 +0100

    daten für tag 3

---
+++ tag3/js/uebungenFnc.js
@@ -0,0 +1,47 @@
+//Übungsfunktionen
+//rückgabe eine fullname (beispiel "Mein Name ist John Doe");
+//Filterung der Kontakte nach alter Kleiner oder gleich 30;
+//Berechnung des Altersdurchschnitts aller Kontakte;
+
+const contacts = [
+    {
+        firstname: "John",
+        lastname: "Doe",
+        address: {
+            city: "New York",
+            street: "5th Avenue",
+            zipcode: "10001"
+        },
+        age: "30"
+    },
+    {
+        firstname: "Jane",
+        lastname: "Smith",
+        address: {
+            city: "Los Angeles",
+            street: "Sunset Boulevard",
+            zipcode: "90028"
+        },
+        age: "25"
+    },
+    {
+        firstname: "Mike",
+        lastname: "Johnson",
+        address: {
+            city: "Chicago",
+            street: "Michigan Avenue",
+            zipcode: "60601"
+        },
+        age: "35"
+    },
+    {
+        firstname: "Emily",
+        lastname: "Davis",
+        address: {
+            city: "Houston",
+            street: "Main Street",
+            zipcode: "77002"
+        },
+        age: "55"
+    }
+];
